2f1995798b0d4d0cf242ab6ccf56af6ddb78396a
snozza <andrewcsnead@gmail.com>
2015-10-19 10:01:32 +0100

    adding basic files

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject functional-tic-tac-toe "0.1.0"
+  :description "Tic-tac-toe with no variables"
+  :url "http://example/com"
+  :licence {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main functional-tic-tac-toe.core)
