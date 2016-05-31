08ea9957e63679f18b6f45520e47fd4498402963
snozza <andrewcsnead@gmail.com>
2016-05-31 11:05:53 +0100

    add all tests including full game

--- test/functional_tic_tac_toe/test_console.clj
+++ test/functional_tic_tac_toe/test_console.clj
@@ -1,4 +1,35 @@
 (ns functional_tic_tac_toe.test_console
   (:use
     [clojure.test]
-    [functional-tic-tac-toe.console]))
+    [functional-tic-tac-toe.console]
+    [functional-tic-tac-toe.core :only [make-board final-message]]))
+
+(defn fake-println [& args])
+
+;;;like before each in mocha
+(use-fixtures :each
+  (fn [f] (binding [user-message fake-println
+                    user-prompt fake-println] (f))))
+
+(defn make-input [coll]
+  (apply str (interleave coll (repeat "\n"))))
+
+(deftest test-human-mover
+  (with-in-str (make-input '(1 2 3))
+    (is (= 0 (human-mover (make-board) "X")))
+    (is (= 1 (human-mover (make-board) "X")))
+    (is (= 2 (human-mover (make-board) "X"))))
+  (with-in-str (make-input '(8 6 4))
+    (is (= 7 (human-mover (make-board) "X")))
+    (is (= 5 (human-mover (make-board) "X")))
+    (is (= 3 (human-mover (make-board) "X")))))
+
+(def actual-winner (ref :not-over))
+(defn mock-final-message [winner]
+  (dosync (ref-set actual-winner winner)))
+
+(deftest full-game
+  (binding [final-message mock-final-message]
+    (with-in-str (make-input '(1 1 9 2 8 3))
+      (-main)
+      (is (= "X" @actual-winner)))))
